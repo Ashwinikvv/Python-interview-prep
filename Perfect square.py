069ef93790a1c566011f91d72b5fc8a0c69506dd
Ashwinikvv <ashwinikv91@gmail.com>
2024-11-29 11:15:19 +0530

    Create Perfect square.py

---
+++ Perfect square.py
@@ -0,0 +1,10 @@
+'''Given a positive integer num, write a function that returns True if num is a perfect square else false '''
+def valid_square(num):
+    square = int(num**0.5)
+    check = square**2==num
+    return check
+
+valid_square(10)
+# False
+valid_square(36)
+# True
