d0d441d4cf94d92fb3f372e4d4a7e13c1a2bd4d5
Ashwinikvv <ashwinikv91@gmail.com>
2024-12-03 17:00:01 +0530

    Update Install_sf.py

--- Install_sf.py
+++ Install_sf.py
@@ -1,16 +1,27 @@
+import subprocess
+import urllib.request
+import os
+
 def install_software(package):
+	"""
+	Function to install a software package.
+	"""
 	try:
 		if package == 'firefox':
-			# Download Firefox installer
 			url = 'https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US'
 			installer_path = 'firefox_installer.exe'
 			urllib.request.urlretrieve(url, installer_path)
-			# Install Firefox silently
 			subprocess.check_call([installer_path, '-ms'])
-			# Delete the installer file
 			os.remove(installer_path)
 		else:
 			subprocess.check_call(['pip', 'install', package])
-		print(f"Successfully installed {package}")
+		print("Successfully installed {package}")
 	except subprocess.CalledProcessError:
-		print(f"Failed to install {package}")
+		print("Failed to install {package}")
+
+
+software_packages = ['numpy', 'pandas', 'matplotlib', 'firefox']
+
+for package in software_packages:
+	if package == "firefox":
+		install_software(package)
