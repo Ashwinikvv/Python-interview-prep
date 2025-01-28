923c0fbae9e4db30ddc7e2afacd3cf8f297bacd8
Ashwinikvv <ashwinikv91@gmail.com>
2025-01-28 18:22:30 +0530

    Create Movezerostoend.py

---
+++ Movezerostoend.py
@@ -0,0 +1,42 @@
+# Python Program to move all zeros to end using two traversals
+
+# Function which pushes all zeros to end
+def pushZerosToEnd(arr):
+    
+    # Count of non-zero elements
+    count = 0  
+
+    # If the element is non-zero, replace the element at
+    # index 'count' with this element and increment count
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[count] = arr[i]
+            count += 1
+
+    # Now all non-zero elements have been shifted to
+    # the front. Make all elements 0 from count to end.
+    while count < len(arr):
+        arr[count] = 0
+        count += 1
+
+if __name__ == "__main__":
+    arr = [1, 2, 0, 4, 3, 0, 5, 0]
+    pushZerosToEnd(arr)
+    for num in arr:
+        print(num, end=" ")
+Given an array of integers arr[], the task is to move all the zeros to the end of the array while maintaining the relative order of all non-zero elements.
+
+Examples: 
+
+Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+Output: arr[] = [1, 2, 4, 3, 5, 0, 0, 0]
+Explanation: There are three 0s that are moved to the end.
+
+
+Input: arr[] = [10, 20, 30]
+Output: arr[] = [10, 20, 30]
+Explanation: No change in array as there are no 0s.
+
+Input: arr[] = [0, 0]
+Output: arr[] = [0, 0]
+Explanation: No change in array as there are all 0s.
