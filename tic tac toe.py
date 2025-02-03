6e53e6c7b856e8408eb45da6f0ef877a01d34825
Ashwinikvv <ashwinikv91@gmail.com>
2025-02-03 15:51:25 +0530

    Create tic tac toe.py

---
+++ tic tac toe.py
@@ -0,0 +1,8 @@
+https://www.shiksha.com/online-courses/articles/tic-tac-game-python/
+
+Nutanix ,they asked tic tac toe with string slicing
+return any(set(arr[x:x+3:3] for x in range(0,7))
+store in 9 arr for 3*3,[1,0,1,1,0,1,0,1,0]
+1,4,7 for col
+ 1,2,3, for row
+ 1,5,9 for diag
