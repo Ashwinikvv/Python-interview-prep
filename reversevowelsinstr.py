043ce93cf55fc4485ee2ef0982706903dbf86334
Ashwinikvv <ashwinikv91@gmail.com>
2025-02-22 20:23:00 +0530

    Create reversevowelsinstr.py

---
+++ reversevowelsinstr.py
@@ -0,0 +1,30 @@
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+ 
+
+Example 1:
+
+Input: s = "IceCreAm"
+
+Output: "AceCreIm"
+
+Explanation:
+
+The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = "aeiouAEIOU"
+        i, j = 0, len(s) - 1
+        cs = list(s)
+        while i < j:
+            while i < j and cs[i] not in vowels:
+                i += 1
+            while i < j and cs[j] not in vowels:
+                j -= 1
+            if i < j:
+                cs[i], cs[j] = cs[j], cs[i]
+                i, j = i + 1, j - 1
+        return "".join(cs)
