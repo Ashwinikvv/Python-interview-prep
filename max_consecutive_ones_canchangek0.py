6e83dacde09ac5c43bab890c5f1f6ef3db708d00
Ashwinikvv <ashwinikv91@gmail.com>
2025-02-23 19:15:09 +0530

    Update max_consecutive_ones_canchangek0.py

--- max_consecutive_ones_canchangek0.py
+++ max_consecutive_ones_canchangek0.py
@@ -1,3 +1,31 @@
+https://www.geeksforgeeks.org/find-zeroes-to-be-flipped-so-that-number-of-consecutive-1s-is-maximized/
+# Python program to find the maximum subarray having 
+# all ones, by exploring all subarrays
+
+def maxOnes(arr, k):
+    res = 0
+    
+    # Exploring all subarrays
+    for i in range(len(arr)):
+        
+        # Counter for zeroes
+        cnt = 0
+        for j in range(i, len(arr)):
+            if arr[j] == 0:
+                cnt += 1
+            
+            # If cnt is less than or equal to k, then  
+            # all zeroes can be flipped to one
+            if cnt <= k:
+                res = max(res, j - i + 1)
+    
+    return res
+
+if __name__ == "__main__":
+    arr = [1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1]
+    k = 2
+    print(maxOnes(arr, k))
+
 [Expected Approach] Using Sliding Window Technique – O(n) Time and O(1) Space
 The idea is to use two pointers, start and end, to mark the start and end points of the current subarray (or window). Initially, both pointers are set at the beginning of the array. Continuously increase the size of the current window by incrementing the end pointer while keeping track of the count of zeros within the window. If at any point the count of zeros exceeds k, shrink the window by incrementing the start pointer until the count is reduced to k. The result will be the maximum length among all windows that contain at most k zeros.
 
